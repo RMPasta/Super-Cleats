f6599caa3d4370642e31571a19678545dd465215
rmpasta <rmpasta.code@gmail.com>
2023-05-16 11:42:04 -0400

    try different gunicorn version for render

---
+++ app/models/items.py
@@ -0,0 +1,36 @@
+# from .db import db, environment, SCHEMA, add_prefix_for_prod
+# from datetime import datetime
+
+# class Item(db.Model):
+#     __tablename__ = "items"
+
+#     if environment == "production":
+#         __table_args__ = {'schema': SCHEMA}
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     price = db.Column(db.Integer)
+#     description = db.Column(db.String, nullable=False)
+#     item_img = db.Column(db.String)
+#     location = db.Column(db.String)
+#     team_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('teams.id')))
+#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+#     created_at = db.Column(db.Date, default=datetime.today)
+#     updated_at = db.Column(db.Date, default=datetime.today)
+
+#     users = db.relationship("User", back_populates="items")
+#     teams = db.relationship("Team", back_populates="items")
+
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'name': self.name,
+#             'price': self.price,
+#             'description': self.description,
+#             'item_img': self.item_img,
+#             'location': self.location,
+#             'team_id': self.team_id,
+#             'user_id': self.user_id,
+#             'created_at': self.created_at,
+#             'updated_at': self.updated_at,
+#         }
