cff8e399ac0186ba0f8074671f82b379a1a8d9c9
RMPasta <rmpasta.code@gmail.com>
2023-06-14 19:50:05 -0400

    add marker hover info and positioning

--- react-app/src/components/Maps/Maps.js
+++ react-app/src/components/Maps/Maps.js
@@ -1,6 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 import { useSelector } from "react-redux";
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 const center = {
   lat: 52.633308139790195,
@@ -8,64 +13,84 @@ const center = {
 };
 
 function Maps({ apiKey }) {
-    const tickets = useSelector((state) => state.ticket.tickets);
-    const [hoveredMarker, setHoveredMarker] = useState(null);
-    const [zoomLevel, setZoomLevel] = useState(8);
-    const [mapCenter, setMapCenter] = useState(center);
-    const [typeId, setTypeId] = useState("roadmap");
+  const tickets = useSelector((state) => state.ticket.tickets);
+  const [hoveredMarkerName, setHoveredMarkerName] = useState(null);
+  const [zoomLevel, setZoomLevel] = useState(8);
+  const [mapCenter, setMapCenter] = useState(center);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [typeId, setTypeId] = useState("roadmap");
 
-      const containerStyle = {
-        width: '100%',
-        height: '92vh',
-      };
+  const containerStyle = {
+    width: "100%",
+    height: "92vh",
+  };
 
-      const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: apiKey,
-      });
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: apiKey,
+  });
 
-      const handleMarkerClick = (location) => {
-        setZoomLevel(18); // Adjust the desired zoom level here
-        setMapCenter(location)
-        setTypeId("satellite")
-      };
+  const handleMarkerClick = (location) => {
+    setZoomLevel(18);
+    setMapCenter(location);
+    setTypeId("satellite");
+  };
 
-      function handleZoomChanged(){
-        setZoomLevel(this.getZoom())
-        if (this.getZoom() >= 10) {
-          setTypeId("roadmap")
-        }
-      }
+  function handleZoomChanged() {
+    setZoomLevel(this.getZoom());
+    if (this.getZoom() >= 10) {
+      setTypeId("roadmap");
+    }
+  }
 
-
-      return (
-        <>
-          {isLoaded && (
-    <GoogleMap
-    mapContainerStyle={containerStyle}
-    mapTypeId={typeId}
-    center={mapCenter}
-    zoom={zoomLevel} // Use the zoomLevel state variable
-    onZoomChanged={handleZoomChanged}
-  >
-    {tickets.map((ticket, index) => {
-      const lat = ticket.location.split(',')[0];
-      const lng = ticket.location.split(',')[1];
-      const location = { lat: parseFloat(lat), lng: parseFloat(lng) };
-      return (
-        <Marker
-          key={index}
-          position={location}
-          onMouseOver={() => setHoveredMarker(ticket)}
-          onMouseOut={() => setHoveredMarker(null)}
-          onClick={() => handleMarkerClick(location)}
-        />
-      );
-    })}
-  </GoogleMap>
+  return (
+    <>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          mapTypeId={typeId}
+          center={mapCenter}
+          zoom={zoomLevel}
+          onZoomChanged={handleZoomChanged}
+        >
+          {tickets.map((ticket, index) => {
+            const lat = ticket.location.split(",")[0];
+            const lng = ticket.location.split(",")[1];
+            const location = { lat: parseFloat(lat), lng: parseFloat(lng) };
+            const currentLocation = {
+              lat: parseFloat(lat) + (zoomLevel > 9 ? 0.03 : 0.15),
+              lng: parseFloat(lng) + 0,
+            };
+            return (
+              <Marker
+                key={index}
+                position={location}
+                onMouseOver={() => {
+                  setHoveredMarkerName(ticket.stadium);
+                  setCurrentLocation(currentLocation);
+                }}
+                onMouseOut={() => {
+                  setHoveredMarkerName(null);
+                  setCurrentLocation(null);
+                }}
+                onClick={() => handleMarkerClick(location)}
+              />
+            );
+          })}
+          {hoveredMarkerName && currentLocation && (
+            <InfoWindow
+              position={currentLocation}
+              anchor={{ x: 200, y: 0 }} // Adjust the x and y values according to your needs
+              onCloseClick={() => setHoveredMarkerName(null)}
+              onClick={() => handleMarkerClick(currentLocation)}
+            >
+              <div>{hoveredMarkerName}</div>
+            </InfoWindow>
           )}
-          </>
-      )
+        </GoogleMap>
+      )}
+    </>
+  );
 }
 
 export default React.memo(Maps);
